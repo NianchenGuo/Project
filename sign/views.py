14b21eefb97e81ce3462d1f895408c9b9fdde6a0
NianchenGuo <18270865236@163.com>
2019-12-14 16:27:58 +0800

    first commit

---
+++ sign/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import HttpResponse,HttpResponseRedirect  # HttpResponseRedirect重定向类
+
+# Create your views here.
+def index(request):
+	return render(request,"index.html")
+
+# 登录动作	
+def login_action(request):
+	if request.method == "POST":
+		username = request.POST.get("username", "")
+		password = request.POST.get("password", "")
+		if username == "admin" and password == "admin123":
+			response = HttpResponseRedirect("/event_manage/")
+			response = set_cookie("user", username, 3600) # 添加浏览器cookie
+			return response
+			
+		else:
+			return render(request, "index.html", {"error": "username or password error!"})
+
+			
+# 发布会管理			
+def event_manage(request):
+	username = request.COOKIES.get("user", "") # 读取浏览器cookie
+	return render(request, "event_manage.html", {"user":username})
+	
+	
+	
